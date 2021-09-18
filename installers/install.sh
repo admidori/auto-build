123d90f63ab8cfe28555c42f28ce0e07ce92aa27
Aoi Kondo <agotadmidori@gmail.com>
2021-09-18 14:04:15 +0900

    installers update. when install/uninstall , make excute docker-compose build/down

--- installers/install.sh
+++ installers/install.sh
@@ -4,5 +4,9 @@ ABD_DIR=${NOW_DIR%/installers}
 
 chmod a+x $ABD_DIR/abd
 ln -si $ABD_DIR/abd /usr/local/bin
+
+cd $ABD_DIR
+echo "Setup docker-compose"
+docker-compose build
 echo "Install complete!"
 echo "Thank you for installing! Welcome to Auto-build(abd)!"
