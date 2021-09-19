9cddcf9a9c31d85265e6f40e201e8bb83ebc21c3
Midori Ado <admidori.develop@gmail.com>
2021-09-19 20:55:47 +0900

    update update.sh

--- installers/update.sh
+++ installers/update.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-NOW_DIR=$(pwd)
-ABD_DIR=${NOW_DIR%/installers}
-source $ABD_DIR/.env
+PULL=$(git pull origin master -q)
+UPDATE_STRING="Already up to date."
 
-echo "Auto-build update..."
-git pull
-docker-compose build
-echo ""
-echo "######### Update complete! #########"
-echo "Update complete!"
-echo "Welcome to Auto-build Version:$VERSION!"
-echo "Check release notes -> https://github.com/rp-agota/auto-build/releases/tag/$VERSION"
+if [ "$PULL" = "" ]; then
+    echo "abd: [ERROR] Already up to date."
+    exit 0
+else
+    NOW_DIR=$(pwd)
+    ABD_DIR=${NOW_DIR%/installers}
+    source $ABD_DIR/.env
+
+    echo "Auto-build update..."
+    docker-compose build
+    echo ""
+    echo "######### Update complete! #########"
+    echo "Update complete!"
+    echo "Welcome to Auto-build Version:$VERSION!"
+    echo "Check release notes -> https://github.com/rp-agota/auto-build/releases/tag/$VERSION"
+fi
