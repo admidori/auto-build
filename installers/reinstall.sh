76d583b3d82c0a0fde86d71afeb1542ec658e066
Aoi Kondo <agotadmidori@gmail.com>
2021-09-19 20:03:17 +0900

    env add

--- installers/reinstall.sh
+++ installers/reinstall.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-echo "Auto-build update..."
+NOW_DIR=$(pwd)
 ABD_DIR=${NOW_DIR%/installers}
+source ABD_DIR/.env
+
+echo "Auto-build update..."
 git pull
 docker-compose build
 echo "Update complete!"
+echo "Welcome to Auto-build Version:$VERSION!"
+echo "Check release notes -> https://github.com/rp-agota/auto-build/releases/tag/$VERSION"
