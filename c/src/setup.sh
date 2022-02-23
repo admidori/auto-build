fdfe574834a59f7ccb59255a9ed75c238647bec7
Midori Ado <admidori.develop@gmail.com>
2022-02-23 15:33:37 +0900

    Update files: remove bugs

--- c/src/setup.sh
+++ c/src/setup.sh
@@ -2,5 +2,6 @@
 gcc $name.c
 if [ $exec -eq 0 ]; then
     ./a.out > output.txt
+else
+		./a.out
 fi
-rm a.out
--- cpp/src/setup.sh
+++ cpp/src/setup.sh
@@ -2,5 +2,6 @@
 g++ $name.cpp
 if [ $exec -eq 0 ]; then
     ./a.out > output.txt
+else
+	./a.out
 fi
-rm a.out
